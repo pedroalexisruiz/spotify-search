527f39c53a96ce25877be40547e66122adb52196
pedroruiz <pedro.ruiz@ceiba.com.co>
2020-11-18 16:37:31 -0500

    feat(main): Se activa CORS

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.REACT_APP_DOMAIN_URL,
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
